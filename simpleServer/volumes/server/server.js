f00b6034ce2d51bc6905378ab104949aa9a2af6c
Fabrice ALCINDOR <fabrice.alcindor@amadeus.com>
2023-02-23 17:20:10 +0100

    Support css

--- simpleServer/volumes/server/server.js
+++ simpleServer/volumes/server/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+var path = require('path');
 
 // Constants
 const PORT = 8080;
@@ -8,8 +9,15 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index', { title: 'Postman Functions Server' });
 });
 
 app.get('/functions', (req, res) => {
